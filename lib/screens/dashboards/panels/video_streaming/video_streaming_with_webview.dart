4fcc171929d5fb6d77fc94089ea92db4dda8b186
Fatih Sarhan <f9n@protonmail.com>
2019-03-22 04:33:21 +0300

    DefaultResourceId ile Resource'u elde et. Sonrasinda resource degeri ile url'i duzelt.

--- lib/screens/dashboards/panels/video_streaming/video_streaming_with_webview.dart
+++ lib/screens/dashboards/panels/video_streaming/video_streaming_with_webview.dart
@@ -1,8 +1,15 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+import 'package:biricik/db/dbHelper.dart';
+import 'package:biricik/models/resource.dart';
+
 class VideoStreamingWithWebView extends StatelessWidget {
+  DbHelper dbHelper = new DbHelper();
+  SharedPreferences sharedPreferences;
   final Completer<WebViewController> _controller =
       Completer<WebViewController>();
 
@@ -11,17 +18,37 @@ class VideoStreamingWithWebView extends StatelessWidget {
 
   VideoStreamingWithWebView({this.width: 200.0, this.height: 200.0});
 
+  Future<Resource> _getResource() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    var defaultResourceId = sharedPreferences.getInt('defaultResourceId');
+    return dbHelper.getResourceById(defaultResourceId);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: width + 5.0,
       height: height + 5.0,
-      child: WebView(
-        initialUrl:
-            "http://192.168.1.101:8080/stream?width=${width}&height=${height}",
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller.complete(webViewController);
+      child: FutureBuilder<Resource>(
+        future: _getResource(),
+        builder: (BuildContext context, AsyncSnapshot<Resource> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return const CircularProgressIndicator();
+            default:
+              if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+
+              print("Snapshot Data => ${snapshot.data}");
+              var initialUrl =
+                  "${snapshot.data.url}/stream?width=${width}&height=${height}";
+              return WebView(
+                initialUrl: initialUrl,
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (WebViewController webViewController) {
+                  _controller.complete(webViewController);
+                },
+              );
+          }
         },
       ),
     );
