e202d8df6a576c1a92f93e7bcb0398273bced5c3
Fatih Sarhan <f9n@protonmail.com>
2019-04-05 06:09:27 +0300

    Update settings screen

--- lib/screens/settings/index.dart
+++ lib/screens/settings/index.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -16,27 +17,59 @@ class SettingsScreenState extends State<SettingsScreen> {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-      body: ListView(
-        children: <Widget>[
-          FutureBuilder<SharedPreferences>(
-            future: _prefs,
-            builder: (BuildContext context,
-                AsyncSnapshot<SharedPreferences> snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return const CircularProgressIndicator();
-                default:
-                  if (snapshot.hasError)
-                    return Text('Error: ${snapshot.error}');
+      body: Center(
+        child: FutureBuilder<SharedPreferences>(
+          future: _prefs,
+          builder: (BuildContext context,
+              AsyncSnapshot<SharedPreferences> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return const CircularProgressIndicator();
+              default:
+                if (snapshot.hasError) return Text('Error: ${snapshot.error}');
 
-                  var defaultResourceId =
-                      snapshot.data.getInt('defaultResourceId') ?? Null;
-                  return Text(
-                      'Default Resource Id: ${defaultResourceId == Null ? 'Not Defined' : defaultResourceId}');
-              }
-            },
-          ),
-        ],
+                var defaultResourceId =
+                    snapshot.data.getInt('defaultResourceId') ?? Null;
+
+                return ListView(
+                  padding: EdgeInsets.all(20.0),
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Default Resource Id',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                            '${defaultResourceId == Null ? 'Not Defined' : defaultResourceId}')
+                      ],
+                    ),
+                    Divider(
+                      color: Colors.black,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Default Dashboard Id',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text('6'),
+                      ],
+                    ),
+                    Divider(
+                      color: Colors.black,
+                    ),
+                  ],
+                );
+            }
+          },
+        ),
       ),
     );
   }
