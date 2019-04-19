27076f566fe0759dc8069b6cbc693cb475725546
Fatih Sarhan <f9n@protonmail.com>
2019-04-19 03:55:54 +0300

    Square widget'in hareket alani kisitlandi.

--- lib/screens/dashboards/panels/square.dart
+++ lib/screens/dashboards/panels/square.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
 class SquareWidget extends StatefulWidget {
+  final double width, height;
+  final double offsetX, offsetY;
+
+  SquareWidget({
+    Key key,
+    this.width = 100.0,
+    this.height = 100.0,
+    this.offsetX = 200.0,
+    this.offsetY = 200.0,
+  }) : super(key: key);
+
   @override
   _SquareState createState() => _SquareState();
 }
 
 class _SquareState extends State<SquareWidget> {
-  Offset offset = Offset(100, 100);
+  Offset offset;
+
+  @override
+  void initState() {
+    super.initState();
+    offset = Offset(widget.width, widget.height);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +35,21 @@ class _SquareState extends State<SquareWidget> {
           var newDxOffset = offset.dx + details.delta.dx;
           var newDyOffset = offset.dy + details.delta.dy;
           print("Dx: $newDxOffset Dy: $newDyOffset");
-          setState(() {
-            offset = Offset(newDxOffset, newDyOffset);
-          });
+          double width = MediaQuery.of(context).size.width;
+          double height = MediaQuery.of(context).size.height;
+          print("Real Width: $width, Height: $height");
+          if ((0 < newDxOffset && newDxOffset < width - widget.width) &&
+              (0 < newDyOffset && newDyOffset < height - widget.height)) {
+            setState(() {
+              offset = Offset(newDxOffset, newDyOffset);
+            });
+          }
         },
-        child: Container(width: 100, height: 100, color: Colors.red),
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.red,
+        ),
       ),
     );
   }
