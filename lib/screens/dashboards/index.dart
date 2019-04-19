b4579036e165742bb96ea1de5ab93ee9f857600d
Fatih Sarhan <f9n@protonmail.com>
2019-04-19 13:57:02 +0300

    Tek bir yerde default resource elde edilir.

--- lib/screens/dashboards/index.dart
+++ lib/screens/dashboards/index.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:biricik/screens/dashboards/panels/video_streaming/index.dart';
+import 'package:biricik/db/dbHelper.dart';
+import 'package:biricik/models/resource.dart';
+
+import 'package:biricik/screens/dashboards/panels/video_streaming.dart';
 import 'package:biricik/screens/dashboards/panels/square.dart';
 import 'package:biricik/screens/dashboards/panels/movement.dart';
 
@@ -10,18 +15,41 @@ class DashboardsScreen extends StatefulWidget {
 }
 
 class DashboardsScreenState extends State<DashboardsScreen> {
+  DbHelper dbHelper = new DbHelper();
+  SharedPreferences sharedPreferences;
+
+  Future<Resource> _getDefaultResource() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    var defaultResourceId = sharedPreferences.getInt('defaultResourceId');
+    return dbHelper.getResourceById(defaultResourceId);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Dashboards"),
       ),
-      body: Stack(
-        children: <Widget>[
-          // VideoStreamingWidget(),
-          SquareWidget(),
-          MovementWidget()
-        ],
+      body: FutureBuilder<Resource>(
+        future: _getDefaultResource(),
+        builder: (BuildContext context, AsyncSnapshot<Resource> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          if (snapshot.hasData) {
+            var url = snapshot.data.url;
+            return Stack(
+              children: <Widget>[
+                // VideoStreamingWidget(url: url),
+                SquareWidget(),
+                MovementWidget(
+                  url: url,
+                ),
+              ],
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
